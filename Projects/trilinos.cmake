178aefdc4678665fbfa9e2eb3d6f160e105a5639
Jamil Appa <jamil.appa@zenotech.com>
2014-12-08 16:40:05 +0000

    Fix trillinos build

--- Projects/trilinos.cmake
+++ Projects/trilinos.cmake
@@ -1,7 +1,7 @@
 
 add_external_project(
   trilinos
-  DEPENDS  mpi parmetis scotch lapack
+  DEPENDS  mpi parmetis lapack #scotch
 
   CMAKE_ARGS
     -DCMAKE_BUILD_TYPE:STRING=Release
@@ -9,15 +9,15 @@ add_external_project(
 	-DCMAKE_CXX_COMPILER:FILEPATH=mpicxx
 	-DBUILD_SHARED_LIBS:BOOL=ON
 	-DTPL_ENABLE_MPI:BOOL=ON
-	-DTPL_ENABLE_Scotch:BOOL=ON
+	-DTPL_ENABLE_Scotch:BOOL=OFF
 	-DTPL_ENABLE_ParMETIS:BOOL=ON
 	-DTPL_METIS_LIBRARIES:FILEPATH=<INSTALL_DIR>/lib/libmetis.so
 	-DTPL_METIS_INCLUDE_DIRS:PATH=<INSTALL_DIR>/include
-	-DTPL_ParMETIS_LIBRARIES:FILEPATH="<INSTALL_DIR>/lib/libparmetis.so;<INSTALL_DIR>/lib/libmetis.so"
-	-DTPL_Scotch_INCLUDE_DIRS:PATH="<INSTALL_DIR>/include"
-	-DTPL_Scotch_LIBRARIES:FILEPATH="<INSTALL_DIR>/lib/libscotch.a;<INSTALL_DIR>/lib/libscotcherr.a;<INSTALL_DIR>/lib/libptscotch.a;<INSTALL_DIR>/lib/libptscotcherr.a"
-	-DTPL_BLAS_LIBRARIES:FILEPATH="<INSTALL_DIR>/lib/libblas.so" 
-	-DTPL_LAPACK_LIBRARIES:FILEPATH="<INSTALL_DIR>/lib/liblapack.so" 
+	-DTPL_ParMETIS_LIBRARIES:STRING="<INSTALL_DIR>/lib/libparmetis.so;<INSTALL_DIR>/lib/libmetis.so"
+	-DTPL_Scotch_INCLUDE_DIRS:PATH=<INSTALL_DIR>/include
+	-DTPL_Scotch_LIBRARIES:STRING="<INSTALL_DIR>/lib/libscotch.a;<INSTALL_DIR>/lib/libscotcherr.a;<INSTALL_DIR>/lib/libptscotch.a;<INSTALL_DIR>/lib/libptscotcherr.a"
+	-DTPL_BLAS_LIBRARIES:FILEPATH=<INSTALL_DIR>/lib/libblas.so
+	-DTPL_LAPACK_LIBRARIES:FILEPATH=<INSTALL_DIR>/lib/liblapack.so 
 	-DTrilinos_ENABLE_Zoltan:BOOL=ON 
 	-DTrilinos_ENABLE_AztecOO:BOOL=ON 
 	-DTrilinos_ENABLE_Epetra:BOOL=ON 
