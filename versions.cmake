106f9125def9ee9352b8e7ceaffcaddee7ed3045
Jamil Appa <jamil.appa@zenotech.com>
2015-01-06 21:48:02 +0000

    Added ubuntu switch to choose newer version of boost

--- versions.cmake
+++ versions.cmake
@@ -112,11 +112,15 @@ add_revision(snakemq
   URL "https://pypi.python.org/packages/source/s/snakeMQ/snakeMQ-1.2.tar.gz"
   URL_MD5 1bf0c94dfc85dfc44428157b80780595)
 
-add_revision(boost
-   URL "http://packages.zenotech.com/boost_1_53_0.tar.bz2"
-   URL_MD5 a00d22605d5dbcfb4c9936a9b35bc4c2)
-#   URL "http://packages.zenotech.com/boost_1_57_0.tar.bz2"
-#   URL_MD5 1be49befbdd9a5ce9def2983ba3e7b76)
+if (UBUNTU)
+  add_revision(boost
+    URL "http://packages.zenotech.com/boost_1_57_0.tar.bz2"
+    URL_MD5 1be49befbdd9a5ce9def2983ba3e7b76)
+else()
+  add_revision(boost
+     URL "http://packages.zenotech.com/boost_1_53_0.tar.bz2"
+     URL_MD5 a00d22605d5dbcfb4c9936a9b35bc4c2)
+endif()
 #  URL "http://www.paraview.org/files/dependencies/boost_1_50_0.tar.gz"
 #  URL_MD5 dbc07ab0254df3dda6300fd737b3f264)
 
