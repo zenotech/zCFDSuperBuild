ca58a8b69d87b6dde0ab80661cae06b9a17e6981
Jamil Appa <jamil.appa@zenotech.com>
2015-01-13 09:14:57 +0000

    New release branch initialised

--- CMake/zcfd_version.cmake
+++ CMake/zcfd_version.cmake
@@ -1,11 +1,11 @@
 # We hardcode the version numbers since we cannot determine versions during
 # configure stage.
-set (zcfd_version_major 0)
-set (zcfd_version_minor 1)
-set (zcfd_version_patch 0)
+set (zcfd_version_major )
+set (zcfd_version_minor )
+set (zcfd_version_patch )
 set (zcfd_version_suffix)
 set (zcfd_version "${zcfd_version_major}.${zcfd_version_minor}")
-set (zcfd_git_tag "develop")
+set (zcfd_git_tag "release-0.2.0")
 if (zcfd_version_suffix)
   set (zcfd_version_long "${zcfd_version}.${zcfd_version_patch}-${zcfd_version_suffix}")
 else()
