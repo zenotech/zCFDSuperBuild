1f4cc137343e6fe8e101bbfda969eb2c8f731d02
James Sharpe <james.sharpe@zenotech.com>
2016-11-01 20:06:49 -0400

    Add ability to build zcfd dependencies only

---
+++ Projects/zcfddeps.cmake
@@ -0,0 +1,8 @@
+
+add_external_project(
+  zcfddeps 
+  DEPENDS  mpi parmetis boost hdf5 threadworker trilinos python paraview paraviewsdk highorder cgns ccmio qt4
+
+  DOWNLOAD_COMMAND ""
+  SOURCE_DIR "../zCFDSuperBuild/zcfddeps"
+)
