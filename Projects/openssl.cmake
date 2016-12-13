f2821e2efeaf18a0032c7108f5d712b41237c4f7
Jamil Appa <jamil.appa@zenotech.com>
2016-12-13 13:35:33 +0000

    Added build for openssl

---
+++ Projects/openssl.cmake
@@ -0,0 +1,8 @@
+
+add_external_project(
+  openssl
+  BUILD_IN_SOURCE 1
+  CONFIGURE_COMMAND <SOURCE_DIR>/config
+                    --prefix=<INSTALL_DIR>
+                    --openssldir=<INSTALL_DIR>
+)
