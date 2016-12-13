ccb4f5587665cb3c13c2ee15bab67b97525ea90e
Jamil Appa <jamil.appa@zenotech.com>
2016-12-13 15:26:26 +0000

    Temporarily removed dependency on openssl

--- Projects/openssl.cmake
+++ Projects/openssl.cmake
@@ -5,6 +5,10 @@ add_external_project(
   CONFIGURE_COMMAND <SOURCE_DIR>/config
                     --prefix=<INSTALL_DIR>
                     --openssldir=<INSTALL_DIR>
+                    zlib enable-camellia enable-seed enable-rfc3779
+                    enable-cms enable-md2 enable-rc5 enable-ssl3 enable-ssl3-method 
+                    no-mdc2 no-ec2m no-gost no-srp 
+                    shared
   #BUILD_COMMAND make
   #INSTALL_COMMAND make install
 )
