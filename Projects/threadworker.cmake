efa87fb5709a3081268536510ffae067d0a4a966
James Sharpe <james.sharpe@zenotech.com>
2017-06-08 10:41:48 +0100

    Add missing dependency

--- Projects/threadworker.cmake
+++ Projects/threadworker.cmake
@@ -2,7 +2,9 @@
 add_external_project(
   threadworker
 
+  DEPENDS boost
+
   CMAKE_ARGS
-    -DCMAKE_BUILD_TYPE:STRING=Release ${CMAKE_EXTRA_ARGS} 
+    -DCMAKE_BUILD_TYPE:STRING=Release ${CMAKE_EXTRA_ARGS}
 )
 
