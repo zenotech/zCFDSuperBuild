a6d829bf2c0e65c7ccd3f11bd92f4148781a00b2
Jamil Appa <jamil.appa@zenotech.com>
2017-06-27 14:36:41 +0100

    Extra options for mvapich build

--- Projects/mpi.cmake
+++ Projects/mpi.cmake
@@ -1,4 +1,30 @@
 
+SET(MPI_EXTRA_CMDS "")
+# Check for presense of hcoll and fca
+SET(HCOLL_FOUND "FALSE")
+execute_process(COMMAND "if [ -d /opt/mellanox/hcoll ]; then echo TRUE; else echo FALSE fi;" OUTPUT_VARIABLE HCOLL_FOUND)
+IF( HCOLL_FOUND STREQUAL "TRUE")
+  SET(MPI_EXTRA_CMDS "${MPI_EXTRA_CMDS} --with-hcoll=/opt/mellanox/hcoll")
+ENDIF()
+if(NOT POWER8)
+  # Check for presense of hcoll and fca
+  SET(FCA_FOUND "FALSE")
+  execute_process(COMMAND "if [ -d /opt/mellanox/fca ]; then echo TRUE; else echo FALSE fi;" OUTPUT_VARIABLE FCA_FOUND)
+  IF( FCA_FOUND STREQUAL "TRUE")
+    SET(MPI_EXTRA_CMDS "${MPI_EXTRA_CMDS} --with-fca=/opt/mellanox/fca")
+  ENDIF()
+endif
+SET(MXM_FOUND "FALSE")
+execute_process(COMMAND "if [ -d /opt/mellanox/mxm ]; then echo TRUE; else echo FALSE fi;" OUTPUT_VARIABLE MXM_FOUND)
+IF( MXM_FOUND STREQUAL "TRUE")
+  SET(MPI_EXTRA_CMDS "${MPI_EXTRA_CMDS} --with-mxm=/opt/mellanox/mxm")
+ENDIF()
+#SET(KNEM_FOUND "FALSE")
+#execute_process(COMMAND "if [ -d \"$(find /opt -maxdepth 1 -type d -name \"knem*\" -print0)\" ] ; then echo TRUE; else echo FALSE fi;" OUTPUT_VARIABLE KNEM_FOUND)
+#IF( KNEM_FOUND STREQUAL "TRUE")
+#  SET(MPI_EXTRA_CMDS "${MPI_EXTRA_CMDS} --with-fca=/opt/mellanox/mxm")
+#ENDIF()
+
 IF(APPLE)
   add_external_project_or_use_system(mpi
   CONFIGURE_COMMAND <SOURCE_DIR>/configure
@@ -18,7 +44,7 @@ elseif(POWER8)
       CONFIGURE_COMMAND <SOURCE_DIR>/configure
                         --prefix=<INSTALL_DIR>
                         --enable-threads=runtime
-
+                        ${MPI_EXTRA_CMDS}
       # PVExternalProject_Add sets up an parallel build, by default.
       # that doesn't work for the version of MPICH2 we're using.
       #BUILD_COMMAND ${CMAKE_MAKE_PROGRAM}
@@ -31,35 +57,11 @@ elseif(POWER8)
 
 ELSE()
 
- if(BUILD_SHARED_LIBS)
-  set(shared_args --enable-shared --disable-static)
-else()
-  set(shared_args --disable-shared --enable-static)
-endif()
-
-  SET(MPI_EXTRA_CMDS "")
-  # Check for presense of hcoll and fca
-  SET(HCOLL_FOUND "FALSE")
-  execute_process(COMMAND "if [ -d /opt/mellanox/hcoll ]; then echo TRUE; else echo FALSE fi;" OUTPUT_VARIABLE HCOLL_FOUND)
-  IF( HCOLL_FOUND STREQUAL "TRUE")
-    SET(MPI_EXTRA_CMDS "${MPI_EXTRA_CMDS} --with-hcoll=/opt/mellanox/hcoll")
-  ENDIF()
-  # Check for presense of hcoll and fca
-  SET(FCA_FOUND "FALSE")
-  execute_process(COMMAND "if [ -d /opt/mellanox/fca ]; then echo TRUE; else echo FALSE fi;" OUTPUT_VARIABLE FCA_FOUND)
-  IF( FCA_FOUND STREQUAL "TRUE")
-    SET(MPI_EXTRA_CMDS "${MPI_EXTRA_CMDS} --with-fca=/opt/mellanox/fca")
-  ENDIF()
-  SET(MXM_FOUND "FALSE")
-  execute_process(COMMAND "if [ -d /opt/mellanox/mxm ]; then echo TRUE; else echo FALSE fi;" OUTPUT_VARIABLE MXM_FOUND)
-  IF( MXM_FOUND STREQUAL "TRUE")
-    SET(MPI_EXTRA_CMDS "${MPI_EXTRA_CMDS} --with-mxm=/opt/mellanox/mxm")
-  ENDIF()
-  #SET(KNEM_FOUND "FALSE")
-  #execute_process(COMMAND "if [ -d \"$(find /opt -maxdepth 1 -type d -name \"knem*\" -print0)\" ] ; then echo TRUE; else echo FALSE fi;" OUTPUT_VARIABLE KNEM_FOUND)
-  #IF( KNEM_FOUND STREQUAL "TRUE")
-  #  SET(MPI_EXTRA_CMDS "${MPI_EXTRA_CMDS} --with-fca=/opt/mellanox/mxm")
-  #ENDIF()
+  if(BUILD_SHARED_LIBS)
+    set(shared_args --enable-shared --disable-static)
+  else()
+    set(shared_args --disable-shared --enable-static)
+  endif()
 
   add_external_project_or_use_system(mpi
     CONFIGURE_COMMAND <SOURCE_DIR>/configure
@@ -73,20 +75,16 @@ endif()
                       --with-openib
                       --with-verbs
                       ${MPI_EXTRA_CMDS}
-                      #--with-hcoll=/opt/mellanox/hcoll
-                      #--with-fca=/opt/mellanox/fca
-                      #--with-mxm=/opt/mellnox/mxm
-                      #--with-knem=$(find /opt -maxdepth 1 -type d -name "knem*" -print0)
 
+    # PVExternalProject_Add sets up an parallel build, by default.
+    # that doesn't work for the version of MPICH2 we're using.
+    #BUILD_COMMAND ${CMAKE_MAKE_PROGRAM}
+    BUILD_IN_SOURCE 1
+    )
 
-  # PVExternalProject_Add sets up an parallel build, by default.
-  # that doesn't work for the version of MPICH2 we're using.
-  #BUILD_COMMAND ${CMAKE_MAKE_PROGRAM}
-  BUILD_IN_SOURCE 1
-)
-if(NOT USE_SYSTEM_mpi)
-  set(MPI_C_COMPILER <INSTALL_DIR>/bin/mpicc)
-  set(MPI_CXX_COMPILER <INSTALL_DIR>/bin/mpic++)
-endif()
+  if(NOT USE_SYSTEM_mpi)
+    set(MPI_C_COMPILER <INSTALL_DIR>/bin/mpicc)
+    set(MPI_CXX_COMPILER <INSTALL_DIR>/bin/mpic++)
+  endif()
 
 ENDIF()
