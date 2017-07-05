b3319b22ddf164075413f4884cf50ba5721f6ceb
Jamil Appa <jamil.appa@zenotech.com>
2017-07-05 19:06:54 +0100

    Added dependency on netcdf c++ interface

--- Projects/pythonrequirements.cmake
+++ Projects/pythonrequirements.cmake
@@ -1,7 +1,7 @@
 
 add_external_project(
   pythonrequirements 
-  DEPENDS  python mpi hdf5 netcdf
+  DEPENDS  python mpi hdf5 netcdf netcdfcpp
 
   DOWNLOAD_COMMAND ""
   SOURCE_DIR "../zCFDSuperBuild/python-requirements"
