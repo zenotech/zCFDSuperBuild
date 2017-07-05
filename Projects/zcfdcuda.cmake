588a792298147ef06dd581321847e25f9cff7128
Jamil Appa <jamil.appa@zenotech.com>
2017-07-05 11:49:12 +0100

    Removed dependency on highorder plugin

--- Projects/zcfdcuda.cmake
+++ Projects/zcfdcuda.cmake
@@ -11,7 +11,7 @@ add_external_project(
   CMAKE_ARGS
     -DBUILD_CUDA:BOOL=ON
 	-DBoost_NO_SYSTEM_PATHS:BOOL=ON
-	-DHDF5_DIR:FILEPATH=<INSTALL_DIR>/share/cmake/hdf5
+	-DHDF5_DIR:FILEPATH=<INSTALL_DIR>/share/cmake
 	-DMPI_VENDOR:STRING=${MPI_VENDOR}
 	${ZCFD_EXTRA_CMAKE_ARGS}
 )
@@ -20,7 +20,7 @@ ELSE()
 
 add_external_project(
   zcfdcuda
-  DEPENDS  mpi parmetis boost hdf5 threadworker trilinos python paraview paraviewsdk highorder cgns ccmio qt4
+  DEPENDS  mpi parmetis boost hdf5 threadworker trilinos python paraview paraviewsdk cgns ccmio qt4
 
   DOWNLOAD_COMMAND ""
   SOURCE_DIR "../zCFD"
@@ -29,7 +29,7 @@ add_external_project(
   CMAKE_ARGS
     -DBUILD_CUDA:BOOL=ON
 	-DBoost_NO_SYSTEM_PATHS:BOOL=ON
-	-DHDF5_DIR:FILEPATH=<INSTALL_DIR>/share/cmake/hdf5
+	-DHDF5_DIR:FILEPATH=<INSTALL_DIR>/share/cmake
 	-DMPI_VENDOR:STRING=${MPI_VENDOR}
 	${ZCFD_EXTRA_CMAKE_ARGS}
 )
--- Projects/zcfddeps.cmake
+++ Projects/zcfddeps.cmake
@@ -1,7 +1,7 @@
 
 add_external_project(
   zcfddeps 
-  DEPENDS  mpi parmetis boost hdf5 threadworker trilinos python paraview paraviewsdk highorder cgns ccmio qt4
+  DEPENDS  mpi parmetis boost hdf5 threadworker trilinos python paraview paraviewsdk cgns ccmio qt4
 
   DOWNLOAD_COMMAND ""
   SOURCE_DIR "../zCFDSuperBuild/zcfddeps"
