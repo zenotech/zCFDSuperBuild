06dae172accdf10b839bf6adcfa714b96e501d33
Jamil Appa <jamil.appa@zenotech.com>
2017-07-06 14:35:12 +0100

    Added patch cmd for cray build

--- Projects/boost.cmake
+++ Projects/boost.cmake
@@ -7,6 +7,8 @@ if(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
   set(bjam_extra_commands toolset=clang linkflags="-Wl,-rpath,${install_location}/lib")
 endif()
 
+set(patch_cmd "echo No patching required")
+
 if(BOOST_INTEL_TOOLSET)
   if(APPLE)
     set(extra_commands --with-toolset=intel-darwin)
@@ -21,8 +23,10 @@ else()
   if(DEFINED ENV{CRAYOS_VERSION})
     set(extra_commands --with-toolset=gcc)
     if($ENV{PE_ENV} STREQUAL "CRAY")
+      set(patch_cmd "echo \"using cray : : CC ; \" >> <SOURCE_DIR>/tools/build/src/user-config.jam")
       set(bjam_extra_commands toolset=cray)
     else()
+      set(patch_cmd "echo \"using gcc : : CC ; \" >> <SOURCE_DIR>/tools/build/src/user-config.jam")
       set(bjam_extra_commands toolset=gcc)
     endif()
   else()  
@@ -40,6 +44,7 @@ endif()
 add_external_project_or_use_system(boost
   DEPENDS zlib bzip2 python pythonrequirements
   BUILD_IN_SOURCE 1
+  PATCH_COMMAND ${CMAKE_COMMAND} -E ${patch_cmd}
   CONFIGURE_COMMAND
     <SOURCE_DIR>/bootstrap.sh --prefix=<INSTALL_DIR>
                               --with-python=<INSTALL_DIR>/bin/python
