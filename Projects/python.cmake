2b4a379497106a1eb3acc75dca287b4f5c671fb5
Jamil Appa <jamil.appa@zenotech.com>
2017-07-11 13:51:39 +0100

    Ensured python is always compiled with gcc

--- Projects/python.cmake
+++ Projects/python.cmake
@@ -8,17 +8,22 @@ else()
   set(shared_args --disable-shared --enable-static)
 endif()
 
+# Note always build python with gcc as disutils tries to
+# detect the compiler to set appropriate flags for native
+# code compilation when installing python packages
+
 add_external_project_or_use_system(python
   DEPENDS bzip2 zlib png openssl
   CONFIGURE_COMMAND <SOURCE_DIR>/configure
                     --prefix=<INSTALL_DIR>
                     --enable-unicode
+                    CC=gcc CXX=g++ FC=gfortran
                     ${shared_args}
   )
 if (NOT CROSS_BUILD_STAGE STREQUAL "CROSS")
   # Pass the -rpath flag when building python to avoid issues running the
   # executable we built.
-  append_flags(LDFLAGS "-Wl,-rpath,${install_location}/lib" PROJECT_ONLY)
+  append_flags(LDFLAGS "-L${install_location}/lib64 -Wl,-rpath,${install_location}/lib -Wl,-rpath,${install_location}/lib64" PROJECT_ONLY)
   append_flags(CPPFLAGS "-I${install_location}/include -I${install_location}/include/openssl" PROJECT_ONLY)
 endif()
 
