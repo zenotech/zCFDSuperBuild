270f8d4472a9357cdf88bd7e10ad86df9f98e952
James Sharpe <james.sharpe@zenotech.com>
2017-11-01 14:37:33 +0000

    Bump version number

--- CMake/zcfd_version.cmake
+++ CMake/zcfd_version.cmake
@@ -2,10 +2,10 @@
 # configure stage.
 set (zcfd_version_major 2017)
 set (zcfd_version_minor 2)
-set (zcfd_version_patch 2)
+set (zcfd_version_patch 3)
 set (zcfd_version_suffix)
 set (zcfd_version "${zcfd_version_major}.${zcfd_version_minor}")
-set (zcfd_git_tag "v2017.2.2")
+set (zcfd_git_tag "v2017.2.3")
 if (zcfd_version_suffix)
   set (zcfd_version_long "${zcfd_version}.${zcfd_version_patch}-${zcfd_version_suffix}")
 else()
