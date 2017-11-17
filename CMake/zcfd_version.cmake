3d7475d80699f3b2b26301c76a080992937fefb6
Jamil Appa <jamil.appa@zenotech.com>
2017-11-17 12:32:35 +0000

    Bumped version to 2017.2.4

--- CMake/zcfd_version.cmake
+++ CMake/zcfd_version.cmake
@@ -2,10 +2,10 @@
 # configure stage.
 set (zcfd_version_major 2017)
 set (zcfd_version_minor 2)
-set (zcfd_version_patch 3)
+set (zcfd_version_patch 4)
 set (zcfd_version_suffix)
 set (zcfd_version "${zcfd_version_major}.${zcfd_version_minor}")
-set (zcfd_git_tag "v2017.2.3")
+set (zcfd_git_tag "v2017.2.4")
 if (zcfd_version_suffix)
   set (zcfd_version_long "${zcfd_version}.${zcfd_version_patch}-${zcfd_version_suffix}")
 else()
