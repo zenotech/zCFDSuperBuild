f740eb0071ac0903d2f74d67c2f45cff2da87707
James Sharpe <james.sharpe@zenotech.com>
2018-01-03 12:03:30 +0000

    Don't rebuild libxsmm everytime

--- Projects/libxsmm.cmake
+++ Projects/libxsmm.cmake
@@ -2,8 +2,8 @@
 add_external_project(
   libxsmm
   BUILD_IN_SOURCE 1
-  CONFIGURE_COMMAND /bin/true
-  BUILD_COMMAND make STATIC=0 -j${PV_MAKE_NCPUS}
+  CONFIGURE_COMMAND sed -i -e "s/cp\\\\ -v/cp\\\\ -uv/g" <SOURCE_DIR>/Makefile
+  BUILD_COMMAND make STATIC=0 PREFIX=<INSTALL_DIR> -j${PV_MAKE_NCPUS}
   INSTALL_COMMAND make STATIC=0 PREFIX=<INSTALL_DIR> -j${PV_MAKE_NCPUS} install
 )
 
